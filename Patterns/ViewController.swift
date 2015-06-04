9d9de15a0ad26f2d3cc918152c9dd86f6edcfe63
cafish <mifish2014b@gmail.com>
2015-06-04 15:21:40 -0700

    Initial Commit

---
+++ Patterns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Patterns
+//
+//  Created by Jiajia Luo on 6/4/15.
+//  Copyright (c) 2015 Jiajia Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
